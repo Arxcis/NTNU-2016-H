730af0a623449a84a66b0db0d17e82a70c616487
Arxcis <jonas.solsvik@gmail.com>
2016-08-27 03:00:58 +0200

    implemented numpy
    calculated primes between 1-10 000 000

--- REA1011 - Matematikk/finn_primtall_in_range.py
+++ REA1011 - Matematikk/finn_primtall_in_range.py
@@ -1,47 +1,84 @@
 
 import math
+import numpy as np
+import time
 # findprimes - simple2.py
 
+#range_start = 1
+range_start = int(input("\nRange start: \n"))
+range_end = int(input("\nRange end: \n"))
 
-set_range_length = int(input("\nWhat range to you want to find primes in?\n"))
+                # list comprehension
 
+primes = []              # list which is to have all the found primes
 
-all_numbers = [x+1 for x in range(0, set_range_length)]
-primes = []
+baseclock = time.clock()
 
+#print('Timex : ', 0, ' ms')
+
+all_numbers = np.arange(range_start, range_end, 2, dtype=np.int)
+
+#print('Timey : ', time.clock() - baseclock, ' ms')
 
 a_prime = all_numbers[0]
 
-while (a_prime < math.sqrt(set_range_length)):
-    
-    print('a prime ' + str(a_prime))
+countertime = 0
+while (a_prime < math.sqrt(range_end)):
+
+    #baseclock = time.clock()
+    #print('\nTest ' + str(countertime) + '\n:')
+    #print('a prime ' + str(a_prime))
     primes.append(a_prime)
-    del all_numbers[0]
+    #print('Time append : ', (time.clock() - baseclock)*1000, ' ms')
+
+    #baseclock = time.clock()
+    all_numbers = np.delete(all_numbers, 0)
+    #print('Time delete : ', (time.clock() - baseclock)*1000, ' ms')
     #print(all_numbers)
 
     if a_prime == 1:
+        primes.append(2)
         pass
 
     else:
-        i=0
-        while(1):
-            try:
-                if all_numbers[i] % a_prime == 0:
-                    del all_numbers[i]
+        #baseclock = time.clock()
+        delete_these = []
+        #print('Time np.zeros : ', (time.clock() - baseclock)*1000, ' ms')
 
-                i += 1
-            except:
-                break
+# -----------------------------------
 
-    print("Primes: " + str(len(primes)))
+#    Here is were most of the time is spent.
+
+        baseclock = time.clock()
+
+        # TEST 1
+        for i in range(0, len(all_numbers)):
+            if all_numbers[i] % a_prime == 0:
+                delete_these.append(i)
+
+
+        # TEST 2
+        #remain = np.remainder(all_numbers, a_prime)
+        #for i in range(0, len(all_numbers)):
+        #    if remain[i] == 0:
+        #        delete_these.append(all_numbers[i])
+
+
+        print('Time for loop : %.2f ms' % ((time.clock() - baseclock)*1000))
+
+# ------------------------------------
+
+        #baseclock = time.clock()
+        all_numbers = np.delete(all_numbers, delete_these)
+        #print('Time delete all num : ', (time.clock() - baseclock)*1000, ' ms')
+
     a_prime = all_numbers[0]
+    countertime += 1
 
 
 primes.extend(all_numbers)
 print("\nNumber of primes: " + str(len(primes)))
 
 print('\nLast prime : ' + str(primes[-1]))
-
-print('\n All primes: \n\n' + str(primes))
 
 
